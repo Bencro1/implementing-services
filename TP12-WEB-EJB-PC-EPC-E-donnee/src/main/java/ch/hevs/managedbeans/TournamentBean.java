9570c11a38ea7f64327b1a617d4dd3b037835652
Poluux <hugoboss82001@hotmail.com>
2025-04-02 12:40:27 +0200

    Creation of web page and manageBean

---
+++ TP12-WEB-EJB-PC-EPC-E-donnee/src/main/java/ch/hevs/managedbeans/TournamentBean.java
@@ -0,0 +1,14 @@
+package ch.hevs.managedbeans;
+
+import java.io.Serializable;
+
+import jakarta.annotation.ManagedBean;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+
+@ManagedBean
+@ViewScoped
+@Named("tournamentBean")
+public class TournamentBean implements Serializable {
+	
+}
