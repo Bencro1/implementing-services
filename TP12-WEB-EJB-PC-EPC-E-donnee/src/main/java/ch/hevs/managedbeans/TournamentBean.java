803aaf9e6a31fc94cb6194214a5417c6ce4d8e9d
Poluux <hugoboss82001@hotmail.com>
2025-04-03 13:17:13 +0200

    Working on manageBean of Tournament

--- TP12-WEB-EJB-PC-EPC-E-donnee/src/main/java/ch/hevs/managedbeans/TournamentBean.java
+++ TP12-WEB-EJB-PC-EPC-E-donnee/src/main/java/ch/hevs/managedbeans/TournamentBean.java
@@ -1,8 +1,14 @@
 package ch.hevs.managedbeans;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import ch.hevs.Entitys.Tournament;
 import jakarta.annotation.ManagedBean;
+import jakarta.annotation.PostConstruct;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
 
@@ -10,5 +16,17 @@ import jakarta.inject.Named;
 @ViewScoped
 @Named("tournamentBean")
 public class TournamentBean implements Serializable {
+	private List<Tournament> tournaments;
+	private List<String> tournamentNames;
+	private TournamentService tournamentService;
 	
+	@PostConstruct
+	public void initialize() throws NamingException {
+		// Use JNDI to inject reference to tournament EJB
+		InitialContext ctx = new InitialContext();
+		tournamentService = (Tournament) ctx.lookup(null);
+		
+		// get tournaments
+		List<Tournament> tournamentList = tournamentService.getTournaments();
+	}
 }
